5f6a008a4494f24512506205e6ec63f4b363a56f
Praneet Rane <praneet.rane@gmail.com>
2021-07-13 23:15:00 +0530

    middleware

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
 const app = express();
+const morgan = require("morgan");
 
 //Bring in routes
-const {getPosts} = require("./routes/post");
+const { getPosts } = require("./routes/post");
+
+const myownMiddelware = (req, res, next) => {
+  console.log("middleware applied!!");
+  next();
+};
+
+// middleware
+app.use(morgan("dev"));
+app.use(myownMiddelware);
 
 app.get("/", getPosts);
 
